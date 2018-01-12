a01c6f4c7bf3e6a7d7a2af92c8128fcc4e162a44
hairui <laodahairui@qq.com>
2018-01-12 17:36:14 +0800

    提交

--- src/main/java/com/fable/enclosure/bussiness/controller/BaseController.java
+++ src/main/java/com/fable/enclosure/bussiness/controller/BaseController.java
@@ -1,15 +1,24 @@
 package com.fable.enclosure.bussiness.controller;
 
+import com.fable.enclosure.bussiness.entity.ResultKit;
 import com.fable.enclosure.bussiness.entity.ServiceRequest;
 import com.fable.enclosure.bussiness.entity.ServiceResponse;
 import com.fable.enclosure.bussiness.service.IBaseService;
 import com.fable.enclosure.bussiness.util.SpringContextUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import java.io.UnsupportedEncodingException;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 /**
  * Created by bll on 2017/9/2.
@@ -32,5 +41,67 @@ public class BaseController {
         serviceRequest.setMethod(method);
         serviceRequest.setRequest(request);
         return baseService.service(serviceRequest);
+    }
+
+    @RequestMapping("upload")
+    @ResponseBody
+    public ServiceResponse upload(@RequestParam("file") CommonsMultipartFile file){
+        return ResultKit.serviceResponse(upload(file));
+    }
+
+    @RequestMapping("/showPic")
+    public void showPic(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("utf-8");
+        String name = request.getParameter("name");
+        String url = System.getProperty("user.dir");
+        String path = url.substring(0, url.lastIndexOf(File.separator)) + File.separator + "user" + File.separator + "uploadFile";
+        FileInputStream inputStream = new FileInputStream(path+File.separator+name);
+        int i = inputStream.available();
+        byte[] buff = new byte[i];
+        inputStream.read(buff);
+        inputStream.close();
+        response.setContentType("image/*");
+        OutputStream out = response.getOutputStream();
+        out.write(buff);
+        out.close();
+    }
+
+    private Map<String, Object> upload(MultipartFile file) {
+
+        Map<String, Object> map = new HashMap<>();
+        String fileName = "";
+        String fileUrl = "";
+
+        if (file.isEmpty()) {
+            map.put("fileName", fileName);
+            map.put("fileUrl", fileUrl);
+            return map;
+        }
+
+
+        //String path = request.getSession().getServletContext().getRealPath("upload");
+        String url = System.getProperty("user.dir");
+        String path = url.substring(0, url.lastIndexOf(File.separator)) + File.separator + "user" + File.separator + "uploadFile";
+        File filePath = new File(path);
+        if (!filePath.exists()) {
+            filePath.mkdirs();
+        }
+        String uuid = UUID.randomUUID().toString();
+        fileName = file.getOriginalFilename();
+        fileUrl = uuid ;
+
+        File tempFile = new File(path, fileUrl);
+
+        try {
+            file.transferTo(tempFile);
+            map.put("fileName", fileName);
+            map.put("fileUrl", fileUrl);
+            return map;
+        } catch (Exception e) {
+            e.printStackTrace();
+            map.put("fileName", fileName);
+            map.put("fileUrl", fileUrl);
+            return map;
+        }
     }
 }
