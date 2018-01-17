dc743f1d4efc7716a318d388d66b6b2c0a7302a3
hairui <laodahairui@qq.com>
2018-01-17 13:26:16 +0800

    修改一点点东西

--- src/main/java/com/fable/enclosure/bussiness/controller/BaseController.java
+++ src/main/java/com/fable/enclosure/bussiness/controller/BaseController.java
@@ -1,26 +1,19 @@
 package com.fable.enclosure.bussiness.controller;
 
-import com.fable.enclosure.bussiness.entity.ResultKit;
 import com.fable.enclosure.bussiness.entity.ServiceRequest;
 import com.fable.enclosure.bussiness.entity.ServiceResponse;
 import com.fable.enclosure.bussiness.service.IBaseService;
 import com.fable.enclosure.bussiness.util.SpringContextUtil;
-import com.fable.enclosure.bussiness.util.Tool;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.*;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.io.UnsupportedEncodingException;
 
 /**
  * Created by bll on 2017/9/2.
